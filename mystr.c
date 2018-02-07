e0ea7e2121312c4117b7a73a21f966f2a76fec2a
root <root@LAPTOP-SOOQ3D1N.localdomain>
2018-02-07 13:35:29 -0400

    Added code for remove_dup_blanks

--- mystr.c
+++ mystr.c
@@ -1,9 +1,25 @@
 #include "mystr.h"
+#include <stdio.h>
 
 char *next_word(char *str, char *word){
+	
 	return '\0';
 }
 
 void remove_dup_blanks(char *str){
-
+	char *p1 = str, *p2 = p1+1;
+	while(*p2){
+		if(*p1 == ' ' && *p2 == ' '){
+			while(*p2){
+				char * tmpp = p2;
+				p2++;
+				*tmpp = *p2;
+				tmpp++;
+			}
+			p2 = p1 + 1;
+		}else{
+			p1++;
+			p2++;
+		}
+	}
 }
