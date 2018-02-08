cab4351cc4d41b545e4637258daaf059d4139093
Mallory MacDonald <mmacdo29@id415m40.cs.unb.ca>
2018-02-08 16:31:07 -0400

    updated main file

--- main.c
+++ main.c
@@ -2,8 +2,30 @@
 #include <stdio.h>
 
 int main(){
-	char str[80];
-	scanf("%s\n", str);
-	printf("%s\n", str);
+	char x[80];
+	scanf("%s", x);
+	//char  x[80] = "this is a      test";
+	printf("%s\n",x);
+	remove_dup_blanks(x);
+	char * w = x;
+        char * y = next_word(x, w);
+	printf("y read:\t %s\n", *y);
+	while(*y !='\0'){
+	  printf("wat");
+		char* index;
+		for(index = w; index != y; index++){
+			printf("%c", *index);
+		}
+		printf("\n");
+		w = y;
+		y=next_word(w, w);
+	}
+	printf("while loop finished\n");
+	char* index;
+	for(index = w; index !=y; index++){
+	  printf("%c", *index);
+	}
+	printf("\n%s", x);
+	printf("\n");
 	return 1;
 }
